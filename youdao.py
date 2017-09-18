dcc28bbac3fbca3b3982c5d08773d8ccaa9ed0c8
zhouyagen <mingqiang_zhou@kingdee.com>
2017-09-18 09:27:28 +0800

    记忆卡

--- youdao.py
+++ youdao.py
@@ -5,19 +5,69 @@ import json;
 en = raw_input("请输入要查询的英语（逗号分隔）：");
 ens = en.split(",");
 youdao = httplib.HTTPConnection(host='dict.youdao.com');
+en = [];
+cn = [];
 for e in ens:
     word=[];
-    youdao.request(method="GET",url="/jsonapi?q="+e+"&keyfrom=deskdict.suggest&dogVersion=1.0&dogui=json&client=deskdict&id=1847079e1fb1589fc&vendor=alading&in=YoudaoDict_6.3.69.4001_alading.1460712704&appVer=6.3.69.8341&appZengqiang=0&abTest=5&le=eng&dicts={\"count\":11,\"dicts\":[[\"ec\"],[\"collins\"],[\"phrs\",\"syno\",\"rel_word\"],[\"auth_sents_part\"],[\"web_search\"],[\"typos\"],[\"collins_part\"]]}&LTH=78");
-    resp = youdao.getresponse();
-    content = resp.read();
-    jsonobj = json.loads(content);
-    #
-    word.append(e+"\n");
-    word.append(jsonobj["ec"]["word"][0]["usphone"]);
-    vgq = jsonobj["ec"]["word"][0]["wfs"];
-    if vgq:
-        for wfs in vgq:
-            word.append(wfs["wf"]["name"]+":"+wfs["wf"]["value"]);
-    print(json.dumps(word,encoding="utf-8", ensure_ascii=False, sort_keys=True));
-    resp.close();
+    cns = "";
+    try:
+        youdao.request(method="GET",url="/jsonapi?q="+e+"&keyfrom=deskdict.suggest&dogVersion=1.0&dogui=json&client=deskdict&id=1847079e1fb1589fc&vendor=alading&in=YoudaoDict_6.3.69.4001_alading.1460712704&appVer=6.3.69.8341&appZengqiang=0&abTest=5&le=eng&dicts={\"count\":11,\"dicts\":[[\"ec\",\"ee\"],[\"collins\"],[\"phrs\",\"syno\",\"rel_word\"],[\"auth_sents_part\"],[\"web_search\"],[\"typos\"],[\"collins_part\"]]}&LTH=78");
+        resp = youdao.getresponse();
+        content = resp.read();
+        jsonobj = json.loads(content);
+        #
+        word.append(e);
+        if "ec" in jsonobj:
+            w1 = jsonobj["ec"]["word"][0];
+            if "usphone" in w1:
+                word.append(w1["usphone"]);
+            else:
+                word.append(w1["phone"]);
+
+            ecword = jsonobj["ec"]["word"][0];
+            if "wfs" in ecword:
+                wfs = ecword["wfs"];
+                if wfs:
+                    state = '';
+                    for wf in wfs:
+                        state = state + wf["wf"]["name"]+":"+wf["wf"]["value"] + ","
+                    word.append(state);
+            if "trs" in ecword:
+                trs = ecword["trs"];
+                if trs:
+                    for tr in trs:
+                        word.append(tr["tr"][0]["l"]["i"][0]);
+                        cns += tr["tr"][0]["l"]["i"][0] + ","
+            ee = jsonobj["ee"]["word"]["trs"];
+            if ee:
+                for tr in ee:
+                    trss = tr["pos"]+" ";
+                    etr = tr["tr"];
+                    for t in etr:
+                        word.append(trss + t["l"]["i"]);
+                        if "similar-words" in t:
+                            word.append(json.dumps(t["similar-words"]).replace("[","").replace("]","").replace("{","").replace("}",""));
+            for v in word:
+                print(v);
+            print("-----------------你好，我是分割线-----------------");
+            en.append(e);
+            cn.append(cns);
+            resp.close();
+    except BaseException:
+        print(e + ":获取翻译出现异常");
+
+print("")
+print("-------------------------记忆提示卡----------------")
+for s in en:
+    print(s);
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+for s in cn:
+    print(s);
+print("---------------------------------------------------")
 youdao.close();
